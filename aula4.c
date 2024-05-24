b5de04c258ae437ec1bbe774762215b62f26b605
Matheus Jucá <matheusrjuca@gmail.com>
2024-05-24 15:09:46 -0300

    eficiencia do codigo

--- aula4.c
+++ aula4.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <locale.h>
 FILE * arquivo; 
-int i;
+int i,j;
 struct NotaAluno
 { 
-    float nota1,nota2,nota3,nota4,media;
+    float nota,media;
     char nome[50];
 };
 
@@ -18,23 +18,14 @@ int main(){
         printf("\n(Aluno %d)\n",i);
         printf("Nome: \n");
         scanf("%s", &aluno[i].nome);
-        printf("Nota 1: \n");
-        scanf("%f", &aluno[i].nota1);
-        printf("Nota 2: \n");
-        scanf("%f", &aluno[i].nota2);
-        printf("Nota 3: \n");
-        scanf("%f", &aluno[i].nota3);
-        printf("Nota 4: \n");
-        scanf("%f", &aluno[i].nota4);
-        
-    
-        fprintf(arquivo,"\n(Aluno %d)\n",i);
         fprintf(arquivo,"Nome : %s\n",aluno[i].nome);
-        fprintf(arquivo,"Nota 1: %.2f\n",aluno[i].nota1);
-        fprintf(arquivo,"Nota 2: %.2f\n",aluno[i].nota2);
-        fprintf(arquivo,"Nota 3 : %.2f\n",aluno[i].nota3);
-        fprintf(arquivo,"Nota 4 : %.2f\n",aluno[i].nota4);
-        fprintf(arquivo,"Média: %.2f\n",(aluno[i].nota1 + aluno[i].nota2 + aluno[i].nota3 + aluno[i].nota4) / 4);
+        for (j = 1; j < 5; j++){
+            printf("Nota [%d]: \n",j);
+            scanf("%f",&aluno[i].nota);
+            fprintf(arquivo,"Nota %d: %.2f\n",j,aluno[i].nota);
+            aluno[i].media = aluno[i].media + aluno[i].nota;
+        }   
+        fprintf(arquivo,"Média: %f\n",aluno[i].media/4);
     }
     fclose(arquivo);
     return 0;
